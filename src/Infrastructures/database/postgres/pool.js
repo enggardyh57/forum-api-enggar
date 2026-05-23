5621b30981a8d1e7356542a50e732613d30928ee
enggardyh5 <enggardyah57@gmail.com>
2026-05-23 21:50:03 +0700

    fix github actions postgres ssl

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -1,10 +1,20 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const testConfig = {
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+};
+
+const pool = process.env.NODE_ENV === 'test'
+  ? new Pool(testConfig)
+  : new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
 
 module.exports = pool;
